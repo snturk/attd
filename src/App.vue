0f82c8f90627cc997a47508dd9dfb523aeff6c4b
snturk <muratcansenturk2000@hotmail.com>
2020-03-16 18:45:51 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,45 @@
 <template>
   <div id="app">
-    
+    <router-view></router-view>
   </div>
     
 </template>
 
+<script>
+//COLORS: #3246B3 #64BEFF #F7F9FE #FF3366
+import router from './router/index.js'
+export default {
+
+  data() {
+    return {
+
+    }
+  },
+  created(){
+    router.push("/login");
+  }
+}
+</script>
+
 <style>
+body{
+background: linear-gradient(45deg, rgba(103,118,199,1) 27%, rgba(255,171,171,1) 100%);
+  background-size: 200%;
+  animation-name: bg;
+  animation-duration: 9s;
+  animation-iteration-count: infinite;
+}
+@keyframes bg {
+  0% {background-position: 100% 50%;}
+  50% {background-position: 00% 50%;}
+  100% {background-position: 100% 50%;}
+}
 #app {
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/views/Login.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="login">
+    <div id="panelContainer">
+      <div id="title">attd</div>
+      <div id="loginPanel">
+        <input type="text" name="" id="unameInput" placeholder="username">
+        <input type="password" name="" id="pwInput" placeholder="password" style="margin-top: 20px">
+        <div id="loginBtn">Login</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style>
+.login{
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#panelContainer{
+  text-align: center;
+  width: fit-content;
+}
+
+#title{
+  color: black;
+  font-size: 37px;
+}
+
+#loginPanel{
+  color: #F7F9FE;
+  width: fit-content;
+  padding: 50px 40px;
+  font-size: 20px;
+  border-radius: 4px;
+  background-color: #FF3366;
+  box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 19px rgba(0, 0, 0, 0.32);
+}
+
+input{
+  display: block;
+  margin: 0 auto;
+  margin: 15px;
+  padding: 5px;
+  font-size: 17px;
+  border-radius: 4px;
+  transition-duration: 300ms;
+}
+input:focus{
+  transform: scale(1.1);
+}
+
+#loginBtn{
+  margin: 0 auto;
+  margin-top: 20%;
+  width: fit-content;
+  padding: 9px;
+  font-size: 19px;
+  border-radius: 7px;
+  border: 1px solid black;
+}
+</style>
