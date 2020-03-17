fd0212ca9df87452082522e934bbcb305129629c
snturk <muratcansenturk2000@hotmail.com>
2020-03-17 22:31:07 +0300

    added firebase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
+import firebase from 'firebase'
+import firebaseConfig from "../key.js";
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
