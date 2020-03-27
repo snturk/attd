7daf34606d372806967f060bcd982406b3012b5c
snturk <muratcansenturk2000@hotmail.com>
2020-03-27 17:03:24 +0300

    added new data
    New functions about read or write the data from firebase.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,17 +7,44 @@
 <script>
 import firebase from 'firebase'
 import router from '../router/index.js'
+import {userInf, ref} from '../database.js'
 export default {
   name: 'Home',
   data() {
     return {
-      username: firebase.auth().currentUser.courses
+      courses: undefined,
+      id: undefined,
+      courseName: "",
+      courseAttd: 0,
+      courseLimit: 0
     }
   },
   methods: {
     signOut() {
-      firebase.auth().signOut()
+      firebase.auth().signOut();
+    },
+    pushCourse(name, lim, attd){
+      var data = {
+        name: name,
+        limit: lim,
+        attd: attd
+      }
+      ref.child(this.id + "/courses").push(data);
+    },
+    getCourses(){
+      userInf.forEach(element => {
+      if(element.username == firebase.auth().currentUser.displayName){
+        this.courses = element.courses
+        this.id = element.id
+      }
+    });
     }
+  },
+  mounted() {
+    this.getCourses();
+  },
+  updated() {
+    this.getCourses();
   },
 }
 </script>
