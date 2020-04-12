289d01e120e061f7fb089027074946d0a40c61f6
snturk <muratcansenturk2000@hotmail.com>
2020-04-12 22:05:35 +0300

    added add drop operations and style changes

--- src/components/Course.vue
+++ src/components/Course.vue
@@ -2,8 +2,14 @@
   <div class="course">
     <div id="name">{{name}}</div>
     <hr>
-    <div id="lim">course limit: {{lim}}</div>
-    <div id="attd">current attd: {{attd}}</div>
+    <div id="lim">course limit: <b>{{lim}}</b></div>
+    <div id="attdContainer" v-if="attd != 0">
+    <div id="attdbox" v-for="(n, index) in attd"></div>
+    </div>
+    <div id="opsContainer">
+      <div class="attdChange" id="attdPlus" v-on:click="changeAttd('+')">+</div>
+      <div class="attdChange" id="attdMinus" v-on:click="changeAttd('-')">-</div>
+    </div>
     <div class="btn" id="delete" v-on:click="removeCourse()">delete</div>
   </div>
 </template>
@@ -24,6 +30,16 @@ export default {
     removeCourse(){
       database.ref("/courses/" + this.username + "/courses/" + this.courseID).remove();
     },
+    changeAttd(cmd){
+      var ref = database.ref("/courses/" + this.username + "/courses/" + this.courseID + "/attd");
+      if(cmd == "+"){
+        ref.set(this.attd + 1);
+      }else if(cmd == "-"){
+        if(this.attd > 0){
+          ref.set(this.attd - 1);
+        }
+      }
+    }
   },
 }
 </script>
@@ -35,8 +51,10 @@ export default {
     padding: 10px;
     border-radius: 7px;
     border: 1.2px solid black;
+    box-shadow: 0 19px 20px rgba(0,0,0,0.30), 0 15px 19px rgba(0, 0, 0, 0.32);
     font-family: 'Open Sans', sans-serif;
     color: black;
+    flex-basis: 20%;
     margin: 12px;
     display: flex;
     flex-direction: column;
@@ -56,9 +74,42 @@ export default {
   #lim{
     font-size: 17px;
     margin-top: 3%;
+    margin-bottom: 3%;
   }
-  #attd{
-    font-size: 17px;
+  #attdContainer{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+  #attdbox{
+    padding: 5px;
+    background-color: black;
+    margin: 5px;
+    margin-bottom: 10%; /* (100-32*3)/2 */
+    margin-top: 10%;
+    border-radius: 50%;
+  }
+  #opsContainer{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  .attdChange{
+    border-radius: 50%;
+    width: 36px;
+    height: 36px;
+    padding: 8px;
+    text-align: center;
+    font-size: 20px;
+    border: 3px solid black;
+    margin: 15px;
+  }
+
+  #attdPlus{
+    background-color: #FF3366;
+  }
+  #attdMinus{
+    background-color: #64BEFF;
   }
 
   #delete{
